4c8b94823c220b3a613d70f8f4ae4df3e0e67c85
khushi-jain-17 <khushisjain33@gmail.com@domain.com>
2023-07-01 15:02:30 +0530

    crudinitialize

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+   def show
+  @post = Post.find(params[:id])
+end
+def create
+    post = Post.new(post_params)
+    post.save
+    
+    redirect_to root_path
+  end
+
+
+def edit
+   @post = Post.find(params[:id])
+end
+def update
+   post = Post.find(params[:id])
+   post.update(post_params)
+   
+   redirect_to root_path
+end
+
+def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path,  status: :see_other
+end
+
+
+  
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
+
+
+  
+
